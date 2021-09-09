afc20ecf130b405c76f45b1402c2e9e6b926972a
Adriano Silva Santos <adriano_santos_1992@hotmail.com>
2021-09-09 18:01:09 -0300

    Criação de consultas agrupadas

--- backend/src/main/java/com/softvendas/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/softvendas/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.softvendas.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.softvendas.dsvendas.dto.SaleDTO;
+import com.softvendas.dsvendas.dto.SaleSuccessDTO;
+import com.softvendas.dsvendas.dto.SaleSumDTO;
 import com.softvendas.dsvendas.entities.Sale;
 import com.softvendas.dsvendas.repositories.SaleRepository;
 import com.softvendas.dsvendas.repositories.SellerRepository;
@@ -20,7 +24,7 @@ public class SaleService {
 	@Autowired
 	private SellerRepository sellerRepository;
 	
-	@Transactional(readOnly = true)
+	@Transactional(readOnly = true) // Anotação utilizada para informar que esta transação não necessita realizar o travamento do banco, pois apenas é realizada a leitura dos dados
 	public Page<SaleDTO> findAll(Pageable pageable){
 		
 		sellerRepository.findAll();
@@ -29,6 +33,16 @@ public class SaleService {
 		
 		return response.map(sale -> new SaleDTO(sale));
 		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return saleRepository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return saleRepository.successGroupBySeller();
 	}
 	
 }
